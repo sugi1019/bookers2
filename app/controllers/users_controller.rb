8129f5fc37ab3a351b2a7667abe5becfe4895f3b
EC2 Default User <ec2-user@ip-172-31-8-143.ap-northeast-1.compute.internal>
2025-02-23 08:01:18 +0000

    [Add] 他ユーザーからのアクセス拒否

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,18 @@ class UsersController < ApplicationController
   end
 
   def edit
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
     @user = User.find(current_user.id)
   end
 
   def update
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
     @user = User.find(params[:id])
     if@user.update(user_params)
       flash[:notice] = "You have updated user successfully."
